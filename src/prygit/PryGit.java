2e9a675edc70ac822b737cdfbf8addf813dbb4dc
Laboratorio4Pc3 <Laboratorio4Pc3@U4-03.UCATEC.local>
2017-08-11 20:16:08 -0400

    ssss

--- src/prygit/PryGit.java
+++ src/prygit/PryGit.java
@@ -15,7 +15,7 @@ public class PryGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here sss
+        // TODO code application logic heresss
     }
     
 }
